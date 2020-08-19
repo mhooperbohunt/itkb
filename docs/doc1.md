28086ee2d673c19d74e56e6f0d546cf86a281652
MacDevH <MacDevH@gmail.com>
2020-08-19 14:01:52 +0100

    updated siteconfig

---
+++ docs/doc1.md
@@ -0,0 +1,6 @@
+---
+id: doc1
+title: Coming Soon
+---
+
+# The user docs will be coming soon
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,34 +1,27 @@
 const siteConfig = {
-  title: 'Bohunt Wokingham IT ', // Title for your website.
+  title: 'Bohunt Wokingham IT ', 
   tagline: 'A Document Site for IT Support at Bohunt Wokingham',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
+  url: 'https://mhooperbohunt.github.io',
+  baseurl: '/itkb',
+  projectName: 'itkb',
+  organizationName: 'mhooperbohunt',
   projectName: 'bohunt-wokingham-docs',
   organizationName: 'Bohunt Wokingham',
-  // For no header links in the top nav bar -> headerLinks: [],
+  
   headerLinks: [
     {doc: 'ad', label: 'Docs'},
-    {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
 
-  /* path to images for header/footer */
   headerIcon: 'img/favicon.ico',
   footerIcon: 'img/favicon.ico',
   favicon: 'img/favicon.ico',
 
-  /* Colors for website */
   colors: {
-    primaryColor: '#aa8c16',
-    secondaryColor: '#76620f',
+    primaryColor: '#000000',
+    secondaryColor: '#f0a806',
   },
 
-  /* Fonts */
   fonts: {
     myFont: [
       "Open Sans ",
@@ -36,27 +29,19 @@ const siteConfig = {
     ]
   },
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} Bohunt Wokingham`,
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'default',
   },
 
-  // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
   onPageNav: 'separate',
-  // No .html extensions for paths.
   cleanUrl: true,
 
-  // Open Graph and Twitter card images.
   ogImage: 'img/undraw_online.svg',
   twitterImage: 'img/undraw_tweetstorm.svg',
-
-  // repoUrl: 'https://github.com/mhooperbohunt/itkb',
 };
 
 module.exports = siteConfig;
